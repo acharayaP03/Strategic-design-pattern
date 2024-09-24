1df3570ecca569017997b857fde4cd889845b480
acharayaP03 <acharyaP03@gmail.com>
2024-09-24 12:29:54 +1000

    making method to take only one responsibility

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ Console.WriteLine(@"Select filters:
 var numbers = new List<int> { 10, 14, -100, 55, 17, 22 };
 
 var userInput = Console.ReadLine();
-List<int> result = new NumbersFilter().FilterBy(userInput, numbers);
+var fiteringStrategy = new FilteringStrategySelector().Select(userInput);
+List<int> result = new NumbersFilter().FilterBy(fiteringStrategy, numbers);
 
 
 Print(result);
@@ -25,28 +26,29 @@ void Print(IEnumerable<int> numbers)
     Console.WriteLine(string.Join(", ", numbers));
 }
 
-
-
-public class NumbersFilter
+public class FilteringStrategySelector
 {
-    public List<int> FilterBy(string filterByType, List<int> numbers)
+    public Func<int, bool> Select(string filterByType)
     {
         switch(filterByType)
         {
             case "Even":
-                return  Select(numbers, number => number % 2 == 0);
+                return number => number % 2 == 0;
             case "Odd":
-               return Select(numbers, number => number % 2 != 0);
+                return number => number % 2 != 0;
                 
             case "Positive":
-                return Select(numbers, number => number > 0);
+                return number => number > 0;
             
             default:
                 throw new NotSupportedException($"{filterByType} is not supported filter at this moment.");
         }
     }
+}
 
-    private List<int> Select(List<int> numbers, Func<int, bool> predicate)
+public class NumbersFilter
+{
+    public List<int> FilterBy(Func<int, bool> predicate, List<int> numbers)
     {
         var result = new List<int>();
 
